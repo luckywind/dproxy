6a5f150cd62e531533eeec5c07d50ebf880b7cb5
luckywind <1318247907@qq.com>
2019-04-20 19:15:07 +0800

    新增cglib动态代理

---
+++ src/main/java/cglib/cgLibTest.java
@@ -0,0 +1,31 @@
+package cglib;
+
+import net.sf.cglib.core.DebuggingClassWriter;
+import net.sf.cglib.proxy.Enhancer;
+
+public class cgLibTest {
+    public static void main(String[] args) {
+
+        // 代理类class文件存入本地磁盘方便我们反编译查看源码
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, ".");
+        // 通过CGLIB动态代理获取代理对象的过程
+        Enhancer enhancer = new Enhancer();
+        // 设置enhancer对象的父类
+        enhancer.setSuperclass(HelloService.class);
+        // 设置enhancer的回调对象
+        enhancer.setCallback(new MyMethodInterceptor());
+        // 创建代理对象
+        HelloService proxy= (HelloService)enhancer.create();
+        // 通过代理对象调用目标方法
+        proxy.sayHello();
+        proxy.sayOthers("world");
+        /**
+         * HelloService 构造
+         * ======插入前置通知========
+         * HelloService:sayHello
+         * ======插入后置通知========
+         * HelloService:sayOthers>>world
+         */
+
+    }
+}
